6f2012106227a47e0f1936243f7aec1ef0513834
Alec Mekarzel <alec.mekarzel@gmail.com>
2022-06-14 23:26:50 -0400

    Fix z-index issues when Popup contains Popover Separate fadeIn from fadeDropIn etc. for shadow overlay in `Popup.jsx` Fix animation rendering too high, especially relevant for shadow

--- src/popover.jsx
+++ src/popover.jsx
@@ -1,23 +1,23 @@
-import { styled, keyframes } from 'goober'
-import React, { createRef, useCallback, useEffect, useState } from 'react'
-import { usePopper } from 'react-popper'
-import useDelayed from 'use-delayed'
-import outsideClick from '@alecmekarzel/outside-click'
-import { RenderToBody } from './portal'
+import { styled, keyframes } from "goober";
+import React, { createRef, useCallback, useEffect, useState } from "react";
+import { usePopper } from "react-popper";
+import useDelayed from "use-delayed";
+import outsideClick from "@alecmekarzel/outside-click";
+import { RenderToBody } from "./portal";
 
-let Wrapper = styled('div', React.forwardRef)`
+let Wrapper = styled("div", React.forwardRef)`
 	z-index: 9998;
 	pointer-events: none;
 
 	&.open {
 		pointer-events: all;
 	}
-`
+`;
 
-let Inner = styled('div')`
+let Inner = styled("div")`
 	opacity: 1;
 	position: relative;
-`
+`;
 
 let fadeIn = keyframes`
 	from {
@@ -29,7 +29,7 @@ let fadeIn = keyframes`
 		opacity: 1;
 		top: -4px;
 	}
-`
+`;
 
 let fadeOut = keyframes`
 	from {
@@ -41,36 +41,41 @@ let fadeOut = keyframes`
 		opacity: 0;
 		top: -8px;
 	}
-`
+`;
 
-export let Popover = React.forwardRef(({ element, children, placement }, ref) => {
-		let [referenceElement, setReferenceElement] = useState(null)
-		let [popperElement, setPopperElement] = useState(null)
-		
+export let Popover = React.forwardRef(
+	({ element, children, placement }, ref) => {
+		let [referenceElement, setReferenceElement] = useState(null);
+		let [popperElement, setPopperElement] = useState(null);
+
 		// Setup popper
-		let { styles, attributes } = usePopper(referenceElement, popperElement, {
-			placement: placement || 'top',
-			modifiers: []
-		})
+		let { styles, attributes } = usePopper(
+			referenceElement,
+			popperElement,
+			{
+				placement: placement || "top",
+				modifiers: [],
+			}
+		);
 
-		let [open, setOpen] = useState(false)
-		let visible = useDelayed(open, 500, [true])
-		let close = useCallback(() => setOpen(false), [setOpen])
+		let [open, setOpen] = useState(false);
+		let visible = useDelayed(open, 500, [true]);
+		let close = useCallback(() => setOpen(false), [setOpen]);
 
-		let popoverEl = element({ visible, open, close }) // Take the element function, pass in props to function for next functional component
+		let popoverEl = element({ visible, open, close }); // Take the element function, pass in props to function for next functional component
 
 		useEffect(() => {
 			return outsideClick(
 				[referenceElement, popperElement],
 				() => setOpen(false),
 				() => open
-			)
-		}, [referenceElement, popperElement, open])
+			);
+		}, [referenceElement, popperElement, open]);
 
 		useEffect(() => {
-			if (!ref) ref = createRef()
-			ref.current = { setOpen }
-		}, [ref, setOpen])
+			if (!ref) ref = createRef();
+			ref.current = { setOpen };
+		}, [ref, setOpen]);
 
 		return (
 			<>
@@ -79,9 +84,9 @@ export let Popover = React.forwardRef(({ element, children, placement }, ref) =>
 					ref={setReferenceElement}
 					onClick={() => setOpen(!open)}
 					style={{
-						width: 'fit-content',
-						height: 'fit-content',
-						display: 'inline-block',
+						width: "fit-content",
+						height: "fit-content",
+						display: "inline-block",
 					}}
 					data-popover-anchor
 				>
@@ -90,14 +95,25 @@ export let Popover = React.forwardRef(({ element, children, placement }, ref) =>
 
 				{visible && (
 					<RenderToBody>
-						<Wrapper className={open ? 'open' : ''} ref={setPopperElement} style={styles.popper} {...attributes.popper}>
-							<Inner style={{ animation: `${open ? fadeIn : fadeOut } 0.1s ease-in-out forwards` }}>
+						<Wrapper
+							className={open ? "open" : ""}
+							ref={setPopperElement}
+							style={styles.popper}
+							{...attributes.popper}
+						>
+							<Inner
+								style={{
+									animation: `${
+										open ? fadeIn : fadeOut
+									} 0.1s ease-in-out forwards`,
+								}}
+							>
 								{popoverEl}
 							</Inner>
 						</Wrapper>
 					</RenderToBody>
 				)}
 			</>
-		)
+		);
 	}
-)
+);
--- src/popup.jsx
+++ src/popup.jsx
@@ -1,11 +1,17 @@
-import { styled, keyframes } from 'goober'
-import React, { createRef, useMemo, useCallback, useEffect, useState } from 'react'
-import { usePopper } from 'react-popper'
-import useDelayed from 'use-delayed'
-import outsideClick from '@alecmekarzel/outside-click'
-import { RenderToBody } from './portal'
+import { styled, keyframes } from "goober";
+import React, {
+	createRef,
+	useMemo,
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
+import { usePopper } from "react-popper";
+import useDelayed from "use-delayed";
+import outsideClick from "@alecmekarzel/outside-click";
+import { RenderToBody } from "./portal";
 
-let Shadow = styled('div')`
+let Shadow = styled("div")`
 	width: 100%;
 	height: 100%;
 	background: rgba(0, 0, 0, 0.4);
@@ -13,114 +19,156 @@ let Shadow = styled('div')`
 	top: 0;
 	left: 0;
 
+	z-index: 9996;
+`;
+let Wrapper = styled("div", React.forwardRef)`
 	z-index: 9997;
-`
-let Wrapper = styled('div', React.forwardRef)`
-	z-index: 9999;
 	pointer-events: none;
 
 	&.open {
 		pointer-events: all;
 	}
-`
+`;
 
-let Inner = styled('div')`
+let Inner = styled("div")`
 	opacity: 1;
 	position: relative;
-`
+`;
 
 let fadeIn = keyframes`
 	from {
 		opacity: 0;
-		top: -8px;
 	}
 
 	to {
 		opacity: 1;
-		top: -4px;
 	}
-`
+`;
 
 let fadeOut = keyframes`
 	from {
 		opacity: 1;
-		top: -4px;
 	}
 
 	to {
 		opacity: 0;
-		top: -8px;
 	}
-`
+`;
 
-export let Popup = React.forwardRef(({ element, children, }, ref) => {
-		let [referenceElement, setReferenceElement] = useState(null)
-		let [popperElement, setPopperElement] = useState(null)
+let fadeDropIn = keyframes`
+	from {
+		opacity: 0;
+		top: -6px;
+	}
 
-		const customModifier = useMemo(() => ({
-			name: 'computeStyles',
+	to {
+		opacity: 1;
+		top: 0px;
+	}
+`;
+
+let fadeDropOut = keyframes`
+	from {
+		opacity: 1;
+		top: 0px;
+	}
+
+	to {
+		opacity: 0;
+		top: -6px;
+	}
+`;
+
+export let Popup = React.forwardRef(({ element, children }, ref) => {
+	let [referenceElement, setReferenceElement] = useState(null);
+	let [popperElement, setPopperElement] = useState(null);
+
+	const customModifier = useMemo(
+		() => ({
+			name: "computeStyles",
 			enabled: true,
-			fn({state}) {
+			fn({ state }) {
 				state.styles.popper = {
 					...state.styles.popper,
-					position: 'fixed',
-					top: '50%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-				}
-		
-				return state
+					position: "fixed",
+					top: "50%",
+					left: "50%",
+					transform: "translate(-50%, -50%)",
+				};
+
+				return state;
 			},
-		}), [])
+		}),
+		[]
+	);
 
-		let { styles, attributes } = usePopper(referenceElement, popperElement, {
-			modifiers: [customModifier]
-		})
-		
-		let [open, setOpen] = useState(false)
-		let visible = useDelayed(open, 500, [true])
-		let close = useCallback(() => setOpen(false), [setOpen])
+	let { styles, attributes } = usePopper(referenceElement, popperElement, {
+		modifiers: [customModifier],
+	});
 
-		let popupEl = element({ visible, open, close })
+	let [open, setOpen] = useState(false);
+	let visible = useDelayed(open, 500, [true]);
+	let close = useCallback(() => setOpen(false), [setOpen]);
 
-		useEffect(() => {
-			return outsideClick(
-				[referenceElement, popperElement],
-				() => setOpen(false), // set open to false on outside click
-				() => open // determine whether popup is enabled
-			)
-		}, [referenceElement, popperElement, open])
+	let popupEl = element({ visible, open, close });
 
-		useEffect(() => {
-			// if there's no ref, create one
-			if (!ref) ref = createRef()
-			ref.current = { setOpen }
-		}, [ref, setOpen])
+	useEffect(() => {
+		return outsideClick(
+			[referenceElement, popperElement],
+			() => setOpen(false), // set open to false on outside click
+			() => open // determine whether popup is enabled
+		);
+	}, [referenceElement, popperElement, open]);
 
-		return (
-			<>
-				<div tabIndex={0} ref={setReferenceElement}
-					onClick={() => setOpen(!open)}
-					style={{
-						width: 'fit-content',
-						height: 'fit-content',
-						display: 'inline-block',
-					}}
-					data-popover-anchor>
-					{children}
-				</div>
+	useEffect(() => {
+		// if there's no ref, create one
+		if (!ref) ref = createRef();
+		ref.current = { setOpen };
+	}, [ref, setOpen]);
 
-				{visible && (
-					<RenderToBody>
-						<Shadow style={{ animation: `${open ? fadeIn : fadeOut} 0.1s ease-in-out forwards` }}/>
-						<Wrapper className={open ? 'open' : ''} ref={setPopperElement} style={styles.popper} {...attributes.popper}>
-							<Inner style={{ animation: `${open ? fadeIn : fadeOut} 0.1s ease-in-out forwards` }}>
-								{popupEl}
-							</Inner>
-						</Wrapper>
-					</RenderToBody>
-				)}
-			</>
-		)
-	}
-)
+	return (
+		<>
+			<div
+				tabIndex={0}
+				ref={setReferenceElement}
+				onClick={() => setOpen(!open)}
+				style={{
+					width: "fit-content",
+					height: "fit-content",
+					display: "inline-block",
+				}}
+				data-popover-anchor
+			>
+				{children}
+			</div>
+
+			{visible && (
+				<RenderToBody>
+					<Shadow
+						style={{
+							animation: `${
+								open ? fadeIn : fadeOut
+							} 0.1s ease-in-out forwards`,
+						}}
+					/>
+					<Wrapper
+						className={open ? "open" : ""}
+						ref={setPopperElement}
+						style={styles.popper}
+						{...attributes.popper}
+					>
+						<Inner
+							style={{
+								animation: `${
+									open ? fadeDropIn : fadeDropOut
+								} 0.1s ease-in-out forwards`,
+							}}
+						>
+							{popupEl}
+						</Inner>
+					</Wrapper>
+				</RenderToBody>
+			)}
+		</>
+	);
+});
